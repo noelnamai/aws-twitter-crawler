928b059a7eda52e0ac0abd83411bda961e9acc5d
noelnamai <noelnamai@yahoo.com>
2019-12-23 14:17:04 -0500

    update

--- crawler.py
+++ crawler.py
@@ -27,7 +27,7 @@ class Crawler(object):
         credentials = args['--credentials-file']
         with open(credentials, "r") as obj:
             data = obj.read()
-        self.date = str(date.today())
+        self.date        = str(date.today())
         self.credentials = json.loads(data)
         self.search_term = args['--search-term']
 
@@ -35,10 +35,10 @@ class Crawler(object):
         #loginto the twitter API and return the api object
         credentials = self.credentials
         api = twitter.Api(
-                consumer_key=credentials["api_key"],
-                consumer_secret=credentials["api_secret_key"],
-                access_token_key=credentials["access_token_key"],
-                access_token_secret=credentials["access_token_secret"]
+                consumer_key        = credentials["api_key"],
+                consumer_secret     = credentials["api_secret_key"],
+                access_token_key    = credentials["access_token_key"],
+                access_token_secret = credentials["access_token_secret"]
                 )
         return api
 
@@ -50,20 +50,20 @@ if __name__ == '__main__':
     #print(twitter.ratelimit.RateLimit())
 
     results = api.GetSearch(
-                lang = "en",
-                count = 10,
+                lang        = "en",
+                count       = 10,
                 return_json = True,
                 result_type = "recent",
-                since = client.date,
-                term = client.search_term
+                since       = client.date,
+                term        = client.search_term
                 )
 
     #statuses = json.dumps(results["statuses"], indent = 4, sort_keys = True)
 
     for status in results["statuses"]:
-        id = status["id"]
+        id         = status["id"]
         created_at = status["created_at"]
-        text = status["text"]
-        symbols = status["entities"]["symbols"]
+        text       = status["text"]
+        symbols    = status["entities"]["symbols"]
 
         print(f"id: {id} \t created_at: {created_at} \t text: {text} \t symbols: {symbols}")
